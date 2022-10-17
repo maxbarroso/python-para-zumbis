a66d2e8a555021cd1fc5ef511ec61568bed89d9c
MaxBarroso <maxmilla@gmail.com>
2022-10-17 20:15:54 -0300

    Primeiro exercicio

---
+++ ex01.py
@@ -0,0 +1,13 @@
+print('Este programa mostra a soma de dois numeros inteiros')
+
+a = input('Digite um numero: ')
+
+b = input('Digite outro numero: ')
+
+c = a + b
+
+#print(c)
+
+
+print('A soma de {} + {} eh igual a {}' .format(a, b, c))
+
